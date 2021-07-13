f4b9bc5a862c58ebdda7102e00131b3f845fa5b8
Bryan C Guner <bryan.guner@gmail.com>
2021-07-13 22:33:24 +0000

    formatting

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,48 +1,65 @@
-const express = require('express');
-const cars = require('./cars-model');
-const { checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require('./cars-middleware');
+const express = require("express");
+const cars = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("./cars-middleware");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    cars.getAll()
-        .then((resp) => {
-            res.status(200).json(resp);
-        }).catch(next);
-})
+  cars
+    .getAll()
+    .then((resp) => {
+      res.status(200).json(resp);
+    })
+    .catch(next);
+});
 
 router.get("/:id", [checkCarId], (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    cars.getById(id)
-        .then((resp) => {
-            res.status(200).json(resp);
-        }).catch(next);
-})
+  cars
+    .getById(id)
+    .then((resp) => {
+      res.status(200).json(resp);
+    })
+    .catch(next);
+});
 
-router.post("/", [checkCarPayload, checkVinNumberValid, checkVinNumberUnique], (req, res, next) => {
+router.post(
+  "/",
+  [checkCarPayload, checkVinNumberValid, checkVinNumberUnique],
+  (req, res, next) => {
     const neoCar = req.body;
 
-    cars.create(neoCar)
-        .then((resp) => {
-            cars.getById(resp)
-                .then(r2 => {
-                    res.status(201).json(r2);
-                }).catch(next);
-        }).catch(next);
-})
+    cars
+      .create(neoCar)
+      .then((resp) => {
+        cars
+          .getById(resp)
+          .then((r2) => {
+            res.status(201).json(r2);
+          })
+          .catch(next);
+      })
+      .catch(next);
+  }
+);
 
 router.use((err, req, res, next) => {
-    const status = err.status || 500;
+  const status = err.status || 500;
 
-    res.status(status).json({
-        message: "Error in car router",
-        err: err.message
-    });
+  res.status(status).json({
+    message: "Error in car router",
+    err: err.message,
+  });
 
-    if (err.message === 0) {
-        next();
-    }
-})
+  if (err.message === 0) {
+    next();
+  }
+});
 
 module.exports = router;
